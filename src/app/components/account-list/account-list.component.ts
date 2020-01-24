c4c037ba6bc3fd7834ac1daa4c1eb3645c9a7af6
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-24 12:57:10 +0700

    Use async pipe

--- src/app/components/account-list/account-list.component.ts
+++ src/app/components/account-list/account-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AccountsService } from "../../services/accounts.service";
-import { Account } from "../../models/accounts.model";
-import { Subscription } from "rxjs";
-import { AsyncState } from "rx-stores";
+
+// https://ultimatecourses.com/blog/angular-ngif-async-pipe
 
 @Component({
   selector: "app-account-list",
@@ -10,20 +9,8 @@ import { AsyncState } from "rx-stores";
   styleUrls: ["./account-list.component.css"]
 })
 export class AccountListComponent implements OnInit {
-  accounts: AsyncState<Account[]>;
-  accountsSubscription: Subscription;
-
-  constructor(private accountsService: AccountsService) {
-    this.accountsSubscription = this.accountsService
-      .getAccountsObservable()
-      .subscribe(accounts => {
-        this.accounts = accounts;
-      });
-  }
+  constructor(private accountsService: AccountsService) {}
+  accounts$ = this.accountsService.getObservable();
 
   ngOnInit() {}
-
-  ngOnDestroy() {
-    this.accountsSubscription.unsubscribe();
-  }
 }
--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -21,7 +21,7 @@ export class AccountsService {
     }
   }
 
-  getAccountsObservable() {
+  getObservable() {
     return this.accounts.getObservable();
   }
 }
